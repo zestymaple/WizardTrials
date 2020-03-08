39da92032063765a50072762cea78af2d2f9258b
vamsi-allampati <3952456+vamsi-allampati@users.noreply.github.com>
2020-03-08 01:38:32 -0500

    Cleanup

--- Assets/Minimap.cs
+++ Assets/Minimap.cs
@@ -13,16 +13,4 @@ public class Minimap : MonoBehaviour
         newPosition.y = transform.position.y;
         transform.position = newPosition;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
