4e20879ccb56cf1c7cd3d6c4f5a3572e6c2d98d9
vamsi-allampati <3952456+vamsi-allampati@users.noreply.github.com>
2020-03-07 21:08:32 -0500

    Add basic minimap

---
+++ Assets/Minimap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform MainCamera;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = MainCamera.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
