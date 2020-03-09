b6c09cc01163ebc01d580511479ee1abf45cd9c6
vamsi-allampati <3952456+vamsi-allampati@users.noreply.github.com>
2020-03-09 00:29:14 -0400

    Richa's Changes

---
+++ Assets/pickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public GameObject pick;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        this.gameObject.SetActive(false);
+    }
+}
