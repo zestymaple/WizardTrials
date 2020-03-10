b9848480e67702cdcab3276a70d7e5b048559693
vamsi-allampati <3952456+vamsi-allampati@users.noreply.github.com>
2020-03-09 21:03:57 -0400

    Juyong's Changes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public GameObject scoreTextObject;
+    int score;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        scoreText = scoreTextObject.GetComponent<Text>();
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    public void Collect(int passedValue, GameObject passedObject)
+    {
+        passedObject.GetComponent<Renderer>().enabled = false;
+        //passedObject.GetComponent<Collider>().enabled = false;
+        //Destroy(passedObject, 1.0f);
+        score = score + passedValue;
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
