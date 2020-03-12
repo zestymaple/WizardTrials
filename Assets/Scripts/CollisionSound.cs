ba174cce7dec4f1f15e6de430fc618fc13ea38df
StandInTheRiver <46364873+StandInTheRiver@users.noreply.github.com>
2020-03-12 07:55:08 -0400

    commit 2

---
+++ Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+
+    public AudioSource tickSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            tickSource.Play();
+          
+        }
+    }
+}
---
+++ Assets/Scripts/pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public int value;
+    public float rotateSpeed;
+    public GameObject pick;
+    public AudioSource tickSource;
+
+
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameManager.instance.Collect(value, gameObject);
+        this.GetComponent<Renderer>().enabled = false;
+        //this.GetComponent<Collider>().enabled = false;
+        tickSource.Play();
+        Destroy(this.gameObject, 1.6f);
+        //this.gameObject.SetActive(false);
+    }
+}
