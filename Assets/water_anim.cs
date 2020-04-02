a16e25cf2ed08f2e9cb594516ab36b2a5c3e313d
StandInTheRiver <46364873+StandInTheRiver@users.noreply.github.com>
2020-04-02 01:21:00 -0400

    Swamp level started about 30 percent done

---
+++ Assets/water_anim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water_anim : MonoBehaviour
+{
+    public bool oncooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (oncooldown == false)
+        {
+            StartCoroutine(cooldown(1.2f));
+        }
+    }
+
+
+    IEnumerator cooldown(float waitTime)
+    {
+        oncooldown = true;
+        yield return new WaitForSeconds(waitTime);
+        Vector3 flip_dir = transform.localScale;
+        flip_dir.x *= -1;
+        transform.localScale = flip_dir;
+        oncooldown = false;
+
+
+    }
+
+}
