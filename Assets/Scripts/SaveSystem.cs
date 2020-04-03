220ee917deaa86c490b2e7de2151fe1d09b336e1
vamsi-allampati <3952456+vamsi-allampati@users.noreply.github.com>
2020-04-02 21:30:42 -0400

    - set default player position - use transform instead of entire player object

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 // Game Settings such as volume and difficulty will be saved from 'Menu' Scene
 public class SaveSystem : MonoBehaviour
 {
-    public GameObject Player;
+    public Transform Player;
 
     // called first
     void OnEnable()
@@ -36,11 +36,11 @@ public class SaveSystem : MonoBehaviour
         Debug.Log("OnDisable");
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
-    
+
     public void SaveGame()
     {
         // Player Position
-        var position = Player.transform.position;
+        var position = Player.position;
         Debug.Log($"Saving Player Position: {position.x}:{position.y}:{position.z}");
         PlayerPrefs.SetFloat("player.location.x", position.x);
         PlayerPrefs.SetFloat("player.location.y", position.y);
@@ -57,18 +57,30 @@ public class SaveSystem : MonoBehaviour
     {
         // Load Save
         Debug.Log("Loading Save");
-        
+
         // Player Position
-        if (PlayerPrefs.HasKey("player.location.x") && PlayerPrefs.HasKey("player.location.y") && PlayerPrefs.HasKey("player.location.z"))
+        if (PlayerPrefs.HasKey("player.location.x") && PlayerPrefs.HasKey("player.location.y") &&
+            PlayerPrefs.HasKey("player.location.z"))
         {
             float x = PlayerPrefs.GetFloat("player.location.x");
             float y = PlayerPrefs.GetFloat("player.location.y");
             float z = PlayerPrefs.GetFloat("player.location.z");
-            
+
             Debug.Log($"Loading Player Position: {x}:{y}:{z}");
-            Vector3 posVec = new Vector3(x,y,z);
-            Player.transform.position = posVec;
+            Vector3 posVec = new Vector3(x, y, z);
+            Player.position = posVec;
         }
+        else
+        {
+            float x = -7.31f;
+            float y = 0.78f;
+            float z = 0f;
+
+            Debug.Log($"Loading DEFAULT Player Position: {x}:{y}:{z}");
+            Vector3 posVec = new Vector3(x, y, z);
+            Player.position = posVec;
+        }
+
         // Keyboard & Controller Settings can be directly checked from the player controller
     }
 }
