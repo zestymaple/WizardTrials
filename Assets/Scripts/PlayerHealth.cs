8ec1580d06b8416f398767f2c6beb53f3ea532ef
Richa <richapatel21@gmail.com>
2020-04-03 14:38:40 -0400

    c# code for healthBar

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    Slider healthBar;
+
+    float maxHealth = 100;
+    float curHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.value = maxHealth;
+        curHealth = healthBar.value;
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "potion-red-1")
+        {
+            healthBar.value += 1.5f;
+            curHealth = healthBar.value;
+        }
+        
+    }
+
+    void Update()
+    {
+        healthText.text = curHealth.ToString() + " %";
+    }
+}
