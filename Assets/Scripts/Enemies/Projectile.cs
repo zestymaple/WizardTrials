0a4375dbce0ab4d1f061ab608ce7fd38f5187cd3
Spooky-Boogy <43327220+Spooky-Boogy@users.noreply.github.com>
2020-04-03 21:34:26 -0400

    sorcerer fireball disappear function

---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float startMovingDistance;
+
+    private Transform player;
+    private Vector2 target;
+    public GameObject OriginalObject;
+
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) < startMovingDistance)
+        {
+            //GameObject clone = (GameObject)GameObject.Instantiate(OriginalObject);
+            this.GetComponent<Renderer>().enabled = true;
+            
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+            if (transform.position.x == target.x && transform.position.y == target.y)
+            {
+               // gameObject.SetActive(false);
+                //StartCoroutine(DestroyProjectile());
+                DestroyProjectile();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    
+    //IEnumerator DestroyProjectile()
+    void DestroyProjectile()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+        
+        //Destroy(this.gameObject);
+
+        /*yield return new WaitForSeconds(1);
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);*/
+    }
+}
---
+++ Assets/Scripts/Enemies/Rangesorcerer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rangesorcerer : MonoBehaviour
+{
+    public float speed;
+    public float startMovingDistance;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+
+    public Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) < startMovingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, 0 * Time.deltaTime);
+
+
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+            else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+            {
+                transform.position = this.transform.position;
+            }
+            else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, 0 * Time.deltaTime);
+            }
+
+            if (timeBtwShots <= 0)
+            {
+                //Quanternion = no rotation for projectile.
+                //Instantiate(projectile, transform.position, Quaternion.identity);
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
+        }
+    }
+}
